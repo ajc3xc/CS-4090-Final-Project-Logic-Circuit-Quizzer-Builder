4d342a7eb4e16b1d39082e6a43459b7fb5659985
Holden-Conaway <150845398+Holden-Conaway@users.noreply.github.com>
2023-11-15 11:59:40 -0600

    Logic Gate Functions complete
    Need to full implement the gates now

--- NAND.gd
+++ NAND.gd
@@ -1,12 +1,12 @@
 extends "res://Gates/drop_down_menu.gd"
 
-const gateType = "OR"
+const gateType = "NAND"
 var input1 = false
 var input2 = false
 #used in inhereited nodes
 func _check_if_type_correct():
 	isCorrect = (selectedType == gateType)
-	if (input1 == 1 || input2 == 1):
-		return true
-	else:
+	if (input1 == 1 && input2 == 1):
 		return false
+	else:
+		return true
--- game_simulator.gd
+++ game_simulator.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+
+var input1 = false
+var input2 = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
