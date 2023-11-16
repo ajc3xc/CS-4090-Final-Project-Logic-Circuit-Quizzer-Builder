e1704c5eba5de03a4961b21067e0a4eb6f9a66f9
Conaway <hjcqcz@umsystem.edu>
2023-11-16 12:43:12 -0600

    Removed Logic from logic sim
    Crazy game called life we playing my jit

--- Gates/NOR.gd
+++ Gates/NOR.gd
@@ -2,12 +2,8 @@ extends "res://Gates/drop_down_menu.gd"
 
 
 var gateType = "NOR"
-var input1 = false
-var input2 = false
+
 #used in inhereited nodes
 func _check_if_type_correct():
 	isCorrect = (selectedType == gateType)
-	if (input1 == 1 || input2 == 1):
-		return false
-	else:
-		return true
+
