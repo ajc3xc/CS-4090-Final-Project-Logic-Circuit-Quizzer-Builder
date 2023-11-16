7fb8b8ac5e899e703e71bdf85b4ff63d629ae329
Conaway <holden.conaway@gmail.com>
2023-11-16 15:21:46 -0600

    Signal Functionality
    Press button

---
+++ Gates Button.gd
@@ -0,0 +1,20 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+#create  a function to create the object mentioned in button
+func _on_Gates_Button_pressed():
+	pass # Replace with function body.
---
+++ Label Button.gd
@@ -0,0 +1,23 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+#create  a function to create the object mentioned in button
+func _on_Label_Button_pressed():
+	print("Label has been pressed")
+	
+	pass # Replace with function body.
