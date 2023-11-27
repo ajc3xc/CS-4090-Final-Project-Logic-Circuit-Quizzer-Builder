e8f3354e6786eb237b2296d09732bec0775269a9
Holden-Conaway <150845398+Holden-Conaway@users.noreply.github.com>
2023-11-27 08:02:01 -0600

    Added documentation
    does not crash but no quite functional

--- SaveLevel.gd
+++ SaveLevel.gd
@@ -10,22 +10,24 @@ var save_name_input: LineEdit
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("pressed", self, "_on_button_pressed")
-
+	#should enable the line editor to recieve name of file 
 	save_name_input = $Button/Level_Namer
 
 func _on_Button_pressed():
 	var scene = get_tree().get_edited_scene_root()
 	
 	if scene:
+		#Prompt user for save name
 		var dialog = FileDialog.new()
 		dialog.mode = FileDialog.MODE_SAVE_FILE
 		dialog.popup_centered()
 		
-		dialog.connect("file_selected", self, "_on_Level_Namer_text_entered", [scene])
+		dialog.connect("file_selected", self, "_on_FileDialog_files_selected", [scene])
 	else:
 		print("Error: Scene is empty")
 
 func _on_FileDialog_files_selected(paths, scene):
+	#saves the scene as a new .tscn file
 	var save_path = paths
 	
 	if not save_path.ends_with(".tscn"):
