4d497705e1dbdde04d5cd410325afd4a6922e5ca
Conaway <holden.conaway@gmail.com>
2023-11-27 15:33:37 -0600

    Load/Save works
    Doesnt look great

--- UI/Load_Button.gd
+++ UI/Load_Button.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
+
+func _on_LineEdit_text_entered(new_text):
+	var scene_name = new_text
+	
+	if scene_name != "":
+		var packed_scene = load("res://"+scene_name+".tscn")
+		var my_scene = packed_scene.instance()
+		add_child(my_scene)
+	else:
+		print("Invalid Scene")
