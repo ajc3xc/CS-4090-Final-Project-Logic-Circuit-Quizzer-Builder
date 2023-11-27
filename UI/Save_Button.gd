9c5aa5cb71ab09ff804f8dfbd01eed3af50a96aa
Conaway <holden.conaway@gmail.com>
2023-11-27 15:22:52 -0600

    Removed Garbage
    Installed king shit

---
+++ UI/Save_Button.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_LineEdit_text_entered(new_text):
+	var new_scene = new_text
+	
+	if new_scene != "":
+		var packed_scene = PackedScene.new()
+		packed_scene.pack(get_tree().get_current_scene())
+		ResourceSaver.save("res://"+new_scene+".tscn", packed_scene)
+	else:
+		print("Error: Couldn't save empty scene")
