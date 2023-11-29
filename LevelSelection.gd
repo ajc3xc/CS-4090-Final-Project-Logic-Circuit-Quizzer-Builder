fc196ac811282c97709cbeeb894e33ab05a58db5
Conaway <hjcqcz@umsystem.edu>
2023-11-28 20:11:17 -0600

    fixed functionality of level selection
    Yeet

--- LevelSelection.gd
+++ LevelSelection.gd
@@ -1,6 +1,6 @@
 extends OptionButton
 
-
+var loop_var = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -27,15 +27,18 @@ func list_files_in_directory(path):
 func _on_LevelSelection_pressed():
 	var files_index = 0
 	var files = list_files_in_directory("res://saves")
-	print(files)
-	for i in files:
-		add_item(files[files_index])
-		files_index = + 1
+	
+	if loop_var == false:
+		for i in files:
+			add_item(files[files_index])
+			files_index = + 1
+		loop_var = true
 	print("Finished Loading all saves")
 
 
 
 
 func _on_LevelSelection_item_selected(index):
-	#get_tree().change_scene("res://")
+	var files = list_files_in_directory("res://saves")
+	get_tree().change_scene("res://"+files[index])
 	pass
